48787d012b2fa1ff8ac5252da030edae29582c57
Robson Ferreira <49348939+robsondmf1234@users.noreply.github.com>
2019-12-18 15:16:49 -0300

    Update FormularioHelper.java
    Comentando o código

--- app/src/main/java/com/example/listaitens/FormularioHelper.java
+++ app/src/main/java/com/example/listaitens/FormularioHelper.java
@@ -9,11 +9,12 @@ import com.example.listaitens.modelo.Produto;
  * Created by Robson on 10/12/2019
  */
 public class FormularioHelper {
-
+    
     private final EditText campoNome;
     private final EditText campoQtdAtual;
     private final EditText campoQtdNecessaria;
-
+    
+    //Passando uma referencia da activity para o FormularioHelper, pode utilizar o findviewByid()
     public FormularioHelper(FormularioActivity activity) {
         campoNome = (EditText) activity.findViewById(R.id.formulario_nome);
         campoQtdAtual = (EditText) activity.findViewById(R.id.formulario_qtdAtual);
@@ -21,7 +22,9 @@ public class FormularioHelper {
     }
 
     public Produto pegaProduto() {
+        //Instanciando um novo Produto
         Produto produto = new Produto();
+        //Setando o nome com o valor obtido da variavel campoNome
         produto.setNome(campoNome.getText().toString());
         produto.setQtdAtual(campoQtdAtual.getText().toString());
         produto.setQtdNecessaria(campoQtdNecessaria.getText().toString());
