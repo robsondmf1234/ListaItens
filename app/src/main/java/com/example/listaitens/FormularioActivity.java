858c5e77dff2bc9565483356e9eb5468b1b020ea
Robson Ferreira <49348939+robsondmf1234@users.noreply.github.com>
2019-12-19 16:09:46 -0300

    Update FormularioActivity.java
    Adicionado comentários

--- app/src/main/java/com/example/listaitens/FormularioActivity.java
+++ app/src/main/java/com/example/listaitens/FormularioActivity.java
@@ -12,32 +12,43 @@ import com.example.listaitens.dao.ProdutoDAO;
 import com.example.listaitens.modelo.Produto;
 
 public class FormularioActivity extends AppCompatActivity {
-
+    
+    //Declarando uma classe para auxiliar na coleta dos dados dos campos 
     private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.formulario);
-
+        
+        //Instanciando objeto da classe FormularioHelper
         helper = new FormularioHelper(this);
 
     }
-
+    
+    //Option menu é o menu que fica na parte superior direita da tela 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        //Devolvendo uma instancia de um menu inflater(utilizado para transformar uma xml em uma view)
         MenuInflater inflater = getMenuInflater();
+        //Inflando o menu_formulario(xml) ,para uma view e inserindo na tela 
         inflater.inflate(R.menu.menu_formulario, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        //Swicth case para identificar o id da opção clicada no menuOptionsMenu
         switch (item.getItemId()) {
+            //caso o o id da opção clicada seja menu_formulario_ok 
             case R.id.menu_formulario_ok:
+                //Instanciado o objeto do formulariohelper
                 Produto produto = helper.pegaProduto();
+                //Instanciado o objeto do produto dao 
                 ProdutoDAO dao = new ProdutoDAO(this);
+                //chamando o método inserir da Classe ProdutoDAO
                 dao.insert(produto);
+                //fechando a conexao com o banco 
                 dao.close();
                 Toast.makeText(FormularioActivity.this, "Produto " + produto.getNome() + " salvo com sucesso!", Toast.LENGTH_SHORT).show();
                 finish();
