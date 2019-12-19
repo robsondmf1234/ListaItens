aac2c5e52ea8010c933f17720599901434b0600c
Robson Ferreira <49348939+robsondmf1234@users.noreply.github.com>
2019-12-18 22:16:19 -0200

    Implementado -Insere Produto no Banco de Dados (melhorado a função)

--- app/src/main/java/com/example/listaitens/ListaItens.java
+++ app/src/main/java/com/example/listaitens/ListaItens.java
@@ -20,21 +20,8 @@ public class ListaItens extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_itens);
-        
-        //Instanciando o produto DAO
-        ProdutoDAO dao = new ProdutoDAO(this);
-        //Declarando uma lista para poder receber os produtos buscado no banco de dados
-        List<Produto> produtos = dao.buscaProdutos();
-        //Fechando a conexao com o banco
-        dao.close();
-        
-        //Recuperando a referencia do xml 
-        ListView listaItens = findViewById(R.id.lista_itens);
-        //Convertendo a lista de String para view com o adapter (referencia da activity,layout para mostrar os dados, fonte dos dados)
-        ArrayAdapter<Produto> adapter = new ArrayAdapter<Produto>(this, android.R.layout.simple_list_item_1, produtos);
-        //Setando o adapter na lista 
-        listaItens.setAdapter(adapter);
-        
+        carregaLista();
+
         //Recuperando a referencia do botao
         Button botaoVaiPraSegunda = findViewById(R.id.btn_segundaActivity);
         //Implementando um Listener no botao 
@@ -45,5 +32,27 @@ public class ListaItens extends AppCompatActivity {
                 startActivity(vaiProFormulario);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregaLista();
+    }
+
+    private void carregaLista() {
+        //Instanciando o produto DAO
+        ProdutoDAO dao = new ProdutoDAO(this);
+        //Declarando uma lista para poder receber os produtos buscado no banco de dados
+        List<Produto> produtos = dao.buscaProdutos();
+        //Fechando a conexao com o banco
+        dao.close();
+
+        //Recuperando a referencia do xml
+        ListView listaItens = findViewById(R.id.lista_itens);
+        //Convertendo a lista de String para view com o adapter (referencia da activity,layout para mostrar os dados, fonte dos dados)
+        ArrayAdapter<Produto> adapter = new ArrayAdapter<Produto>(this, android.R.layout.simple_list_item_1, produtos);
+        //Setando o adapter na lista
+        listaItens.setAdapter(adapter);
     }
 }
