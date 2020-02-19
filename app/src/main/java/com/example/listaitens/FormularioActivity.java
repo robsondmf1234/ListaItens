fda8c426cda7bc939951516b602c9e8281a9e8ad
Robson Ferreira <49348939+robsondmf1234@users.noreply.github.com>
2020-02-18 21:25:29 -0300

    Código com novas features Implementado a função de tirar foto - OK

--- app/src/main/java/com/example/listaitens/FormularioActivity.java
+++ app/src/main/java/com/example/listaitens/FormularioActivity.java
@@ -1,16 +1,23 @@
 package com.example.listaitens;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.example.listaitens.dao.ProdutoDAO;
 import com.example.listaitens.modelo.Produto;
+
+import java.io.File;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -26,13 +33,25 @@ public class FormularioActivity extends AppCompatActivity {
         //Instanciando objeto da classe FormularioHelper
         helper = new FormularioHelper(this);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         Produto produto = (Produto) intent.getSerializableExtra("produto");
 
         if (produto != null) {
             helper.preencheFormulario(produto);
         }
 
+        Button botaoFoto = findViewById(R.id.formulario_botao_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                String caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                Uri fotoURI = FileProvider.getUriForFile(FormularioActivity.this, BuildConfig.APPLICATION_ID + ".provider", arquivoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT,fotoURI);
+                startActivity(intentCamera);
+            }
+        });
     }
 
     //Option menu é o menu que fica na parte superior direita da tela 
